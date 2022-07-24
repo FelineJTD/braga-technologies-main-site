4a48b04e7414817d427b10a45f90ee9b63ad95f9
FelineJTD <16520335@std.stei.itb.ac.id>
2022-07-24 17:07:57 +0700

    sections 0-2.5

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      // this differs from the developer handout, but I think it's safer
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      'tt-hoves': ['TT Hoves', 'sans-serif'],
+      'inter': ['Inter', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
