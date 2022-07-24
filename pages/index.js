16350132d6d5f82821654a6a817eed0a61e70132
FelineJTD <16520335@std.stei.itb.ac.id>
2022-07-24 18:16:40 +0700

    sections 3-footer

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
 
+import CardSolution from '../components/card-solution'
+
 // Partner Logos
 import LogoA from '../assets/images/partner-logos/logo-a.png'
 import LogoB from '../assets/images/partner-logos/logo-b.png'
@@ -61,6 +63,43 @@ export default function Home() {
       "author_title": "Director, Land and Geospatial, World Bank.",
     }
   ]
+
+  const examples = [
+    {
+      "title": "Si-Pulau", 
+      "desc": "Technologies that integrate, process, analyze, and visualize big data for your collective's solution.",
+      "type": "GeoDashboard",
+    }, {
+      "title": "Fleet Management System",
+      "desc": "Technologies that integrate, process, analyze, and visualize big data for your collective's solution.",
+      "type": "Enterprise",
+    }, {
+      "title": "Visitor Monitoring",
+      "desc": "Technologies that integrate, process, analyze, and visualize big data for your collective's solution.",
+      "type": "Braga-AI",
+    }, {
+      "title": "West Java Tourism",
+      "desc": "Technologies that integrate, process, analyze, and visualize big data for your collective's solution..",
+      "type": "GeoDashboard",
+    }
+  ]
+
+  const services = [
+    {
+      "title": "GIS",
+      "desc": "Data Collection / GeoMapping Tools / Spatial Research / Data Collections / ETC",
+    }, {
+      "title": "Development",
+      "desc": "Full-Stack Web & Mobile / Product Cycle Management / CMS / Artificial Intelligence / GIS Integration / ETC",
+    }, {
+      "title": "Design",
+      "desc": "Design System / Prototyping / Interaction Design / Digital Platform Design / Kits / Experience Design",
+    }, {
+      "title": "Services",
+      "desc": "Lorem / Ipsum Dolor / Sit Amet Const / ETC",
+    }
+  ]
+
 
   // ** quotes **
   const [activeQuote, setActiveQuote] = useState(0)
@@ -127,7 +166,7 @@ export default function Home() {
         </section>
 
         {/* 01 */}
-        <section className='defaultPadding flex flex-col lg:grid lg:grid-cols-12'>
+        <section className='whiteBG'>
           <p className='font-bold mb-4'>01.</p>
           <div className='col-span-4'>
             <p className='mb-4 font-bold'>Powered by Braga</p>
@@ -137,23 +176,14 @@ export default function Home() {
           <div className='flex flex-col space-y-1 col-span-6 md:grid grid-cols-2 px-0 lg:px-6 gap-6 mt-8'>
             {
               solutions.map((solution, index) => (
-                <div key={index} className='flex flex-col space-y-2 w-full rounded-lg p-3 border-2 border-gray-200'>
-                  <div className='bg-gray-200 w-full h-24'/>
-                  <h5>{solution.title}</h5>
-                  <p className='text-sm'>{solution.desc}</p>
-                  <div className='min-h-[40px] flex-grow' />
-                  <div className='flex justify-between justify-self-end'>
-                    <button className='border-black'>See Solution</button>
-                    <p>{solution.type}</p>
-                  </div>
-                </div>
+                <CardSolution solution={solution} key={index} />
               ))
             }
           </div>
         </section>
 
         {/* 02 */}
-        <section className='bg-gray-900 text-white md:mx-6 rounded-2xl defaultPadding flex flex-col items-start lg:grid lg:grid-cols-12'>
+        <section className='blackBG'>
           <p className='font-bold mb-4'>02.</p>
           <div className='col-span-4'>
             <p className='mb-4 font-bold'>GeoDashboard</p>
@@ -185,7 +215,7 @@ export default function Home() {
         </section>
 
         {/* 03 */}
-        <section className='defaultPadding flex flex-col lg:grid lg:grid-cols-12'>
+        <section className='whiteBG'>
           <p className='font-bold mb-4'>03.</p>
           <p className='mb-4 font-bold col-span-3'>GeoSpatial Potential</p>
           {/* Experts' Quotes */}
@@ -213,14 +243,94 @@ export default function Home() {
           </div>
         </section>
 
-        {/* 04 */}
-        <section className='defaultPadding flex flex-col lg:grid lg:grid-cols-12'>
+        {/* Sectors */}
+        <section className='whiteBG'>
           
+        </section>
+
+        {/* 04 */}
+        <section className='blackBG'>
+          <p className='font-bold mb-4'>04.</p>
+          <div className='col-span-4'>
+            <p className='mb-4 font-bold'>Our Works</p>
+            <h2 className='mb-4'>Braga Solutions, Integrated to You.</h2>
+          </div>
+          <p className='col-span-3 col-start-7 md:self-end mb-4'>Our interdisciplinary design and development approach accelerates the geospatial tech delivery to your hands.</p>
+          <button className='col-span-2 md:self-end mb-4'>Works</button>
+
+          <div className='col-start-2 col-span-11 flex space-x-3 overflow-auto'>
+            {
+              examples.map((example, index) => (
+                <CardSolution solution={example} isDarkMode={true} key={index} />
+              ))
+            }
+          </div>
+          {/* TODO: change to icon */}
+          <div className='col-start-2 col-span-10'>
+            <button className='mr-3'>&lt;</button>
+            <button>&gt;</button>
+          </div>
+
+          <div className='col-start-2 col-span-10 mt-11'>
+            <h4>What We Do</h4>
+            <div className='flex flex-col space-y-1 w-full md:grid grid-cols-2 px-0 gap-4 mt-8'>
+            {
+              services.map((service, index) => (
+                <div key={index} className='flex border-2 border-gray-700 rounded-lg p-3 space-x-4 min-w-[35vw]'>
+                  <div>
+                    <h5 className='font-bold'>{service.title}</h5>
+                    <p className='text-sm'>{service.desc}</p>
+                  </div>
+                </div>
+              ))
+            }
+            </div>
+          </div>
+        </section>
+
+        {/* 05 */}
+        <section className='whiteBG'>
+          <p className='font-bold mb-4'>05.</p>
+          <div className='col-span-4'>
+            <p className='mb-4 font-bold'>Get in Touch</p>
+            <h2 className='mb-4'>Start Mapping Your Future, Today.</h2>
+          </div>
+          <p className='col-span-3 col-start-7 md:self-end mb-4'>Find out how you can maximize the value from data and strengthen your decision making.</p>
+          <button className='col-span-2 md:self-end mb-4'>Contact Us</button>
+
+          <div className='col-start-2 col-span-10 h-96 w-full bg-gray-200 my-6 rounded-xl' />
         </section>
       </main>
 
-      <footer>
-        Footer
+      <footer className='p-6 pt-0 flex flex-col lg:grid lg:grid-cols-12'>
+        <div className='col-span-3'>
+          <p>Indonesia&apos;s Leading Geospatial Technology that optimize analytics and performance, moving the world one map at a time.</p>
+        </div>
+        <div className='col-start-5 col-span-2 pr-6'>
+          <h6>Product</h6>
+          <a href='#'><p>GeoDashboard</p></a>
+          <a href='#'><p>Braga Enterprise</p></a>
+          <a href='#'><p>Braga AI</p></a>
+        </div>
+        <div className='col-span-2 pr-6'>
+          <h6>Works</h6>
+          <a href='#'><p>Works</p></a>
+          <a href='#'><p>Case Study</p></a>
+          <a href='#'><p>Portfolio</p></a>
+        </div>
+        <div className='col-span-2 pr-6'>
+          <h6>Office</h6>
+          <a href='#'><p>About</p></a>
+          <a href='#'><p>Team</p></a>
+          <a href='#'><p>Career</p></a>
+          <a href='#'><p>Contact Us</p></a>
+        </div>
+
+        <div className='col-start-1 col-span-12 border-t-[1px] border-gray-200 mt-10 mb-4' />
+        <a href='#' className='col-start-5 col-span-2'>Privacy</a>
+        <a href='#' className='col-span-2'>Terms of Use</a>
+        <p className='text-right col-span-4'>© Braga Technologies 2022</p>
+        <div className='col-start-1 col-span-12 border-t-[1px] border-gray-200 mt-4' />
       </footer>
     </div>
   )
