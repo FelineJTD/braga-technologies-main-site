12f7384bc6eb9339d5f49da1cbea81ab82e25956
FelineJTD <16520335@std.stei.itb.ac.id>
2022-07-26 18:15:36 +0700

    change a href to link href

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Navbar() {
@@ -7,11 +6,11 @@ export default function Navbar() {
     <nav className='flex items-center justify-between w-full px-6 py-3 bg-white text-gray-600 bg-opacity-80 backdrop-blur sticky top-0 z-40'>
       <img src='/logo-braga-full.svg' alt='Braga Technologies Logo' className='h-6' />
       <div className='justify-self-center space-x-6'>
-        <a href='/' className='un text-sm'><p>Home</p></a>
-        <a href='https://geodashboard.braga.co.id/' rel='noreferrer noopener' className='un text-sm'><p>GeoDashboard</p></a>
-        <a href='/' className='un text-sm'><p>Works</p></a>
-        <a href='/' className='un text-sm'><p>Company</p></a>
-        <a href='/' className='un text-sm'><p>Contact</p></a>
+        <Link href='/'><a className='un text-sm'>Home</a></Link>
+        <a href='https://geodashboard.braga.co.id/' rel='noreferrer noopener' className='un text-sm'>GeoDashboard</a>
+        <Link href='/'><a className='un text-sm'>Works</a></Link>
+        <Link href='/'><a className='un text-sm'>Company</a></Link>
+        <Link href='/'><a className='un text-sm'>Contact</a></Link>
       </div>
       <button className='buttonLight text-gray-900 bg-white bg-opacity-50'>Map Demo</button>
     </nav>
