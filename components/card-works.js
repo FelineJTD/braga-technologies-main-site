2a0a7ad97154269afc7c5cedeb6823f5130cb9f8
FelineJTD <16520335@std.stei.itb.ac.id>
2022-08-05 23:31:54 +0700

    move works card to a separate component

---
+++ components/card-works.js
@@ -0,0 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+import WorkIcon from '../assets/images/works/work-icon';
+import { useState } from 'react';
+
+export default function CardWorks({ work }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div className='w-full rounded-lg border-[1px] border-gray-500 relative' onMouseOver={() => setIsHovered(true)} onMouseOut={() => setIsHovered(false)}>
+      {/* Title */}
+      <div className='p-3 relative z-10'>
+        <WorkIcon className={`${isHovered? 'fill-white' : 'fill-gray-500'} duration-500`} />
+        <h6 className={`${isHovered? 'text-white':'text-gray-700'} mb-1 duration-500`}>{work.title}</h6>
+        <p className={`${isHovered? 'text-white':'text-gray-500'} text-xs duration-500`}>{work.type}</p>
+      </div>
+      {/* Image */}
+      <img src={work.img} alt='' className={`${isHovered? 'h-full' : 'h-72'} w-full rounded-lg object-cover object-bottom absolute z-0 bottom-0 duration-500`} />
+      <div className={`${isHovered? 'opacity-100 h-full' : 'opacity-0 h-72'} bg-gray-900 bg-opacity-50 w-full bottom-0 rounded-lg z-0 duration-500 absolute flex flex-col items-center justify-end p-3 text-white`}>
+        <div className={`${isHovered? 'translate-y-0': 'translate-y-2'} w-full duration-500`}>
+          <button className='buttonDark w-full mb-2 text-sm' disabled={work.isComingSoon}>{work.isComingSoon? 'Coming Soon' : 'View Work'}</button>
+          <div className='flex justify-between w-full'>
+            <p className='text-xs font-normal'>{work.coordinates}</p>
+            <p className='text-xs font-normal'>{work.number}</p>
+          </div>
+        </div>
+      </div>
+      <div className='h-72 w-full' />
+    </div>
+  )
+}
