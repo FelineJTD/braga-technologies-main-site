12a7f6506a63d6bac87a9a0628d17077676c8261
FelineJTD <16520335@std.stei.itb.ac.id>
2022-08-05 14:27:55 +0700

    try changing numbers as window is being scrolled

--- pages/geodashboard.js
+++ pages/geodashboard.js
@@ -1,17 +1,26 @@
 import Head from 'next/head'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import PartnerLogos from '../components/partner-logos'
 
 export default function Home() {
+  const [num, setNum] = useState('01.');
   useEffect(() => {
     const onScroll = () => {
-      console.log(document.getElementById('scroll-area').scrollTop);
+      const top = window.outerHeight+500;
+      if (window.scrollY < top + 300) {
+        setNum('01.');
+      } else if (window.scrollY > top + 300 && window.scrollY < top + 600) {
+        setNum('02.');
+      } else if (window.scrollY > top + 900) {
+        setNum('03.');
+      }
     }
-    document.getElementById('scroll-area').addEventListener('scroll', onScroll);
+    window.addEventListener('scroll', onScroll);
     return () => {
-      document.getElementById('scroll-area').removeEventListener('scroll', onScroll);
+      window.removeEventListener('scroll', onScroll);
     }
   }, []);
+  
 
   return (
     <div>
@@ -33,27 +42,29 @@ export default function Home() {
         <PartnerLogos />
 
         {/* 01 */}
-        <section className='whiteBG pb-0'>
-          <p className='bold mb-4 z-10'>01.</p>
-          <div className='col-span-4 z-10'>
-            <p className='mb-4 bold'>Visualize Easily</p>
-            <h2>From <i>seeing</i> where, to understand why.</h2>
-          </div>
-          <p className='text-gray-600 lg:self-end col-start-7 col-span-4 z-10'>Created with less time, easy to add data, interactive visualization and user friendly interface.</p>
-          <div className='col-start-2 col-span-10 border-t-[1px] border-gray-600 mt-5 mb-10 z-10' aria-hidden/>
-        </section>
-
-        {/* 01 SCROLL AREA */}
-        <section id='scroll-area' className='whiteBG pt-0 -mt-10 flex relative h-[50vh] overflow-scroll'>
-          <div className='flex flex-col justify-between h-[50vh] col-start-2 col-span-4'>
-            <h3 className='font-inter text-[13.5rem] [line-height:10rem] font-medium text-gray-100'>01.</h3>
-            <div className='self-end'>
-              <h5 className='text-2xl mb-4'>Multi-Format Data Visualization</h5>
-              <p>GeoDashboard easily visualize data in point, polygon and polyline format. Inspect rich data display for each attribute.</p>
+        <div className='h-[2500px]'>
+          <section id='scroll-area' className='whiteBG sticky top-0'>
+            <p className='bold mb-4 z-10'>01.</p>
+            <div className='col-span-4 z-10'>
+              <p className='mb-4 bold'>Visualize Easily</p>
+              <h2>From <i>seeing</i> where, to understand why.</h2>
             </div>
-          </div>
-          <div className='col-start-7 col-span-5 row-span-2 bg-gray-200 h-[100vh] w-full rounded-xl' />
-        </section>
+            <p className='text-gray-600 lg:self-end col-start-7 col-span-4 z-10'>Created with less time, easy to add data, interactive visualization and user friendly interface.</p>
+            <div className='col-start-2 col-span-10 border-t-[1px] border-gray-600 mt-5 mb-10 z-10' aria-hidden/>
+
+            {/* 01 SCROLL AREA */}
+            <div className='flex flex-col justify-end min-h-[50vh] h-full col-start-2 col-span-4 relative'>
+              <h3 className='font-inter text-[13.5rem] [line-height:10rem] font-medium text-gray-100 absolute top-0 z-0'>{num}</h3>
+              <div className='self-end relative z-10'>
+                <h5 className='text-2xl mb-4'>Multi-Format Data Visualization</h5>
+                <p>GeoDashboard easily visualize data in point, polygon and polyline format. Inspect rich data display for each attribute.</p>
+              </div>
+            </div>
+            <div className='col-start-7 col-span-5 row-span-2 bg-gray-200 h-full w-full rounded-xl' />
+          </section>
+        </div>
+
+        
 
         {/* 02 */}
         <section className='blackBG'>
