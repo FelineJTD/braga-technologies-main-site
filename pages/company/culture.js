aede4d6ccfdf72e03add962523ecc795c3ef8455
FelineJTD <16520335@std.stei.itb.ac.id>
2022-08-12 16:13:22 +0700

    add culture page

--- pages/company/culture.js
+++ pages/company/culture.js
@@ -11,7 +11,15 @@ export default function Culture() {
       </Head>
 
       <main className='text-gray-900 bg-gray-50 w-full'>
-
+        {/* 00 */}
+        <section className='whiteBG'>
+          <p className='bold mb-4'>00.</p>
+          <p className='col-span-4 mb-4'>Company / <b>Culture</b></p>
+          <h1 className='row-start-2 col-start-4 col-span-6 mb-6'>Locate your <u>uncharted</u> passion here.</h1>
+          <p className='row-start-3 col-start-2 col-span-2'>Values</p>
+          <p className='row-start-3 text-sm text-gray-600 col-span-3'>We&apos;re creating an environment to keep everyone standout with their talents and be able to cherish each and every unique qualities that we have.</p>
+          <p className='row-start-3 text-sm text-gray-600 col-span-3'>Dare to act, own the work, be the truest you. Seizing opportunities daily, that’s how we do.</p>
+        </section>
       </main>
     </div>
   )
