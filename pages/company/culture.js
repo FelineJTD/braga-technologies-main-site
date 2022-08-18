1e989481058fa20fb7dfabcd1810a6f0ff2c1129
FelineJTD <16520335@std.stei.itb.ac.id>
2022-08-18 22:20:10 +0700

    add team component

--- pages/company/culture.js
+++ pages/company/culture.js
@@ -1,10 +1,70 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from 'next/head'
 import Link from 'next/link';
+import TeamIcon from '../../assets/images/company/team-icon';
+import { useState } from 'react';
 
 export default function Culture() {
   const Departments = ['Management', 'Design', 'Development', 'Technology', 'Operation', 'General Affairs', 'Others'];
   const lastSectionImage = "https://picsum.photos/1080/1920";
+  const [isHovered, setIsHovered] = useState(false);
+
+  const Team = [
+    {
+      name: 'Gunawan Wibisono',
+      position: 'IoT Engineer',
+      dept: 'Management',
+      desc: 'When she’s not thinking about taxes and quotations, she’s thinking about which Indonesia’s mountain peak or sunny beach to walk on next.',
+      linkedin: 'https://www.linkedin.com/in/gunawan-wibisono-a9a8b917/',
+      social: 'https://www.instagram.com/gunawan-wibisono-a9a8b917/',
+      img: 'https://picsum.photos/112/112',
+    },
+    {
+      name: 'Gunawan Wibisono',
+      position: 'IoT Engineer',
+      dept: 'Management',
+      desc: 'When she’s not thinking about taxes and quotations, she’s thinking about which Indonesia’s mountain peak or sunny beach to walk on next.',
+      linkedin: 'https://www.linkedin.com/in/gunawan-wibisono-a9a8b917/',
+      social: 'https://www.instagram.com/gunawan-wibisono-a9a8b917/',
+      img: 'https://picsum.photos/112/112',
+    },
+    {
+      name: 'Gunawan Wibisono',
+      position: 'IoT Engineer',
+      dept: 'Management',
+      desc: 'When she’s not thinking about taxes and quotations, she’s thinking about which Indonesia’s mountain peak or sunny beach to walk on next.',
+      linkedin: 'https://www.linkedin.com/in/gunawan-wibisono-a9a8b917/',
+      social: 'https://www.instagram.com/gunawan-wibisono-a9a8b917/',
+      img: 'https://picsum.photos/112/112',
+    },
+    {
+      name: 'Gunawan Wibisono',
+      position: 'IoT Engineer',
+      dept: 'Management',
+      desc: 'When she’s not thinking about taxes and quotations, she’s thinking about which Indonesia’s mountain peak or sunny beach to walk on next.',
+      linkedin: 'https://www.linkedin.com/in/gunawan-wibisono-a9a8b917/',
+      social: 'https://www.instagram.com/gunawan-wibisono-a9a8b917/',
+      img: 'https://picsum.photos/112/112',
+    },
+    {
+      name: 'Gunawan Wibisono',
+      position: 'IoT Engineer',
+      dept: 'Management',
+      desc: 'When she’s not thinking about taxes and quotations, she’s thinking about which Indonesia’s mountain peak or sunny beach to walk on next.',
+      linkedin: 'https://www.linkedin.com/in/gunawan-wibisono-a9a8b917/',
+      social: 'https://www.instagram.com/gunawan-wibisono-a9a8b917/',
+      img: 'https://picsum.photos/112/112',
+    },
+    {
+      name: 'Gunawan Wibisono',
+      position: 'IoT Engineer',
+      dept: 'Design',
+      desc: 'When she’s not thinking about taxes and quotations, she’s thinking about which Indonesia’s mountain peak or sunny beach to walk on next.',
+      linkedin: 'https://www.linkedin.com/in/gunawan-wibisono-a9a8b917/',
+      social: 'https://www.instagram.com/gunawan-wibisono-a9a8b917/',
+      img: 'https://picsum.photos/112/112',
+    },
+  ]
 
   return (
     <div>
@@ -48,6 +108,34 @@ export default function Culture() {
               <p className='text-lg bold mb-1'>Gunawan Wibisono</p>
               <p className='text-xs text-gray-500'>IoT Engineer</p>
             </div>
+          
+          { Team.map((staff, index) => (
+            <div key={index} className={`w-full rounded-lg border-[1px] border-gray-500 relative`} onMouseOver={() => setIsHovered(true)} onMouseOut={() => setIsHovered(false)}>
+              {/* Image */}
+              <img src={staff.img} alt='' className={`${isHovered? 'h-full' : 'h-60'} w-full rounded-lg object-cover object-top absolute z-0 top-0 duration-500`} />
+              <div className={`${isHovered? 'opacity-100 h-full' : 'opacity-0 h-60'} bg-gray-900 bg-opacity-50 w-full top-0 rounded-lg z-0 duration-500 absolute flex flex-col items-center justify-start p-3 text-white`}>
+                <div className={`${isHovered? 'translate-y-0': '-translate-y-2'} w-full duration-500`}>
+                  <div className='flex justify-between w-full space-x-3'>
+                    <p className='text-xs font-normal'>{staff.desc}</p>
+                    <p className='text-xs font-normal'>About</p>
+                  </div>
+                </div>
+              </div>
+              <div className='h-60 w-full' />
+              {/* Title */}
+              <div className='p-3 relative z-10 bottom-0'>
+                <TeamIcon className={`${isHovered? 'fill-white' : 'fill-gray-500'} duration-500`} />
+                <h6 className={`${isHovered? 'text-white':'text-gray-700'} my-1 duration-500`}>{staff.name}</h6>
+                <p className={`${isHovered? 'text-white':'text-gray-500'} mb-1 text-xs duration-500`}>{staff.position}</p>
+                { isHovered && (
+                  <div className='flex justify-between'>
+                    <a href={staff.linkedin} className='text-white text-xs'>Linkedin</a>
+                    <a href={staff.social} className='text-white text-xs'>Social Link</a>
+                  </div>
+                ) }
+              </div>
+            </div>
+          ))}
           </div>
         </section>
 
