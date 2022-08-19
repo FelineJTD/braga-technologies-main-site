93b867ae961e9a81d23844055527d0612f077a72
FelineJTD <16520335@std.stei.itb.ac.id>
2022-08-19 18:51:08 +0700

    improve contact us responsiveness

--- pages/contact.js
+++ pages/contact.js
@@ -200,25 +200,27 @@ export default function Contact() {
           <p className='bold mb-4'>00.</p>
           <p className='col-span-4 mb-4'>Contact</p>
           <h1 className='row-start-2 col-start-4 col-span-6 mb-6'>Get in touch with us.</h1>
-          <p className='row-start-3 col-start-2 col-span-2'>Contact Us</p>
+          <p className='row-start-3 col-start-2 col-span-2 mb-3'>Contact Us</p>
           <h2 className='row-start-3 col-span-3'>How might we help you?</h2>
           <p className='row-start-3 text-sm text-gray-600 col-span-3 mt-3'>See how Braga Technologies could help solve your problem through our geospatial knowledge and approach. </p>
 
           <div className='dividerBlack col-start-4 col-span-6 w-full mt-8' />
 
-          <div className='md:col-start-4 md:col-span-6 md:grid md:grid-cols-4 md:gap-2 flex space-x-2 overflow-auto'>
-            {/* TYPES */}
+          {/* TYPES */}
+          <div className='md:col-start-4 md:col-span-6 md:grid md:grid-cols-4 md:gap-2 flex space-x-2 overflow-auto w-full pb-3 md:pb-0'>
             { Forms.map((form, index) => (
               <button key={index} className={`${currForm.title === form.title? 'buttonSelectionSelected' : 'buttonSelection'} text-sm whitespace-nowrap`} onClick={() => setCurrForm(form)}>{form.title}</button>
             ))}
-            
+          </div>
+
+          <div className='md:col-start-4 md:col-span-6 md:grid md:grid-cols-4 md:gap-3 flex flex-col space-y-2 w-full'>
             {/* INFO */}
             <div className='col-start-1 col-span-4 mt-6 mb-4 rounded-lg border-[1px] border-gray-200 bg-gray-100 p-3'>
               <p className='text-sm'>{currForm.info}</p>
             </div>
 
             {/* FORM */}
-            <form className='col-start-1 col-span-4 grid grid-cols-4 gap-3'>
+            <form className='md:col-start-1 md:col-span-4 md:grid md:grid-cols-4 md:gap-3 flex flex-col space-y-2'>
               {currForm.form.map((field, index) => (
                 <div key={index} className={`w-full col-span-${field.size}`} style={{columnSpan: field.size}}>
                   <label className='text-sm'>{field.label}<br/></label>
@@ -234,16 +236,16 @@ export default function Contact() {
 
         {/* 01 */}
         <section className='px-6 py-14 lg:px-[10%] h-screen bg-cover bg-center bg-no-repeat flex flex-col md:flex-row items-end justify-end' style={{backgroundImage: `url(${lastSectionImage})`}}>
-          <div className='w-full md:w-1/2 md:mb-4 flex md:flex-col items-end'>
+          <div className='w-full md:w-1/2 md:mb-4 flex flex-col items-end pl-[12%] md:pl-0'>
             <img src='/company/additional.svg' alt='' className='-mb-0.5 select-none mr-[12%] w-[20%]' />
-            <div className='flex lg:flex-row flex-col lg:space-x-4 bg-gray-50 rounded-2xl p-6'>
+            <div className='flex lg:flex-row flex-col lg:space-x-4 bg-gray-50 rounded-2xl p-6 w-full'>
               <p className='bold mb-4'>01.</p>
               <div>
                 <p className='mb-4 bold'>Visit us at</p>
-                <h3 className='mb-4 w-2/3'>Braga Technologies Bandung Office</h3>
-                <div className='flex w-full justify-between'>
-                  <p className='text-sm'>Jl. Cilaki No. 23<br />Bandung, Indonesia.<br />40114</p>
-                  <button className='buttonLight w-2/3 lg:w-auto whitespace-nowrap mr-3'>See on Map &gt;</button>
+                <h3 className='mb-4 w-full md:w-2/3'>Braga Technologies Bandung Office</h3>
+                <div className='flex flex-col md:flex-row w-full justify-between'>
+                  <p className='text-sm mb-4 w-full'>Jl. Cilaki No. 23<br />Bandung, Indonesia.<br />40114</p>
+                  <button className='buttonLight lg:w-auto whitespace-nowrap mr-3'>See on Map &gt;</button>
                 </div>
               </div>
             </div>
