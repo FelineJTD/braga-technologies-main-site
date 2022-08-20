40174e58b316a23ceee75fc4528d628302108142
FelineJTD <16520335@std.stei.itb.ac.id>
2022-08-20 21:21:21 +0700

    add not found page

--- pages/404.js
+++ pages/404.js
@@ -2,9 +2,16 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div className='text-center'>
-      <h1>Page Not Found</h1>
-      <Link href="/"><button className='un'>Back to homepage</button></Link>
+    <div className='whiteBG'>
+      <div className='col-span-12 min-h-[60vh]'>
+        
+      </div>
+      <h1 className='col-span-3 self-end'>404.</h1>
+      <h1 className='col-span-6 self-end'>Not Found.</h1>
+      <p className='col-span-3 text-sm self-end mb-4'>
+        What you’re looking for is out of the map.
+        Please return to <Link href="/"><a className='text-primary unPrimary text-sm'>home</a></Link> page.
+      </p>
     </div>
   )
 }
