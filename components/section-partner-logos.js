d1bd932c344b09979af44a8ee01991930c45c464
FelineJTD <16520335@std.stei.itb.ac.id>
2022-09-05 15:51:07 +0700

    add partner logos config

--- components/section-partner-logos.js
+++ components/section-partner-logos.js
@@ -1,19 +1,11 @@
 import Image from 'next/image'
-// Partner Logos
-import LogoA from '../assets/images/partner-logos/logo-a.png'
-import LogoB from '../assets/images/partner-logos/logo-b.png'
-import LogoC from '../assets/images/partner-logos/logo-c.png'
-import LogoD from '../assets/images/partner-logos/logo-d.png'
-import LogoE from '../assets/images/partner-logos/logo-e.png'
-import LogoF from '../assets/images/partner-logos/logo-f.png'
+import { Logos } from '../config/cfg-partner-logos'
 
 export default function PartnerLogos() {
-  const partnerLogos = [LogoA, LogoB, LogoC, LogoD, LogoE, LogoF]
-
   return (
     <section className='px-6 xl:px-[10%] py-6 flex justify-center items-center w-full space-x-3 md:space-x-12'>
       {
-        partnerLogos.map((logo, index) => (
+        Logos.map((logo, index) => (
           <div key={index}>
             <Image
               src={logo}
---
+++ config/cfg-partner-logos.js
@@ -0,0 +1,10 @@
+// CONFIG FOR PARTNER LOGOS SECTION
+
+export const Logos = [
+  require('../assets/images/partner-logos/logo-a.png'),
+  require('../assets/images/partner-logos/logo-b.png'),
+  require('../assets/images/partner-logos/logo-c.png'),
+  require('../assets/images/partner-logos/logo-d.png'),
+  require('../assets/images/partner-logos/logo-e.png'),
+  require('../assets/images/partner-logos/logo-f.png'),
+]
