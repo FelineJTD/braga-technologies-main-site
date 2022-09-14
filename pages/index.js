d9ddc9e76bf00946d6ebdf1437d1bae5375bed91
FelineJTD <16520335@std.stei.itb.ac.id>
2022-09-14 10:01:49 +0700

    move sectors to separate component

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import {
   Solutions, 
   Features, 
   Quotes, 
-  Sectors, 
   Examples, 
   Services 
 } from '../config/cfg-homepage'
@@ -21,8 +20,8 @@ import PartnerLogos from '../components/section-partner-logos'
 import CardSolution from '../components/card-solution'
 import Carousel from '../components/carousel'
 import GetInTouch from '../components/section-get-in-touch'
-import CardSector from '../components/card-sector'
 import Fleet from '../components/fleet'
+import SectorsSection from '../components/section-sectors'
 
 export default function Home() {
   const featureRef = useRef();
@@ -39,17 +38,6 @@ export default function Home() {
       });
     }
   }
-
-  const [selectedSectorIdx, setSelectedSectorIdx] = useState(0);
-  const [sectorLength, _setSectorLength] = useState(Sectors.length);
-  const sectorsRef = useRef(null);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSelectedSectorIdx((selectedSectorIdx + 1)%sectorLength);
-    }, 5000);
-    return () => clearInterval(interval);
-  }, [sectorLength, selectedSectorIdx]);
 
   // Fleet
   const [isVisible1, setIsVisible1] = useState(true);
@@ -134,7 +122,7 @@ export default function Home() {
         <PartnerLogos />
 
         {/* 01 */}
-        <section id='section1' className='whiteBG'>
+        <section id='section1' className='whiteBG relative z-20'>
           <p className='bold mb-4'>01.</p>
           <div className='col-span-4'>
             <p className='mb-4 bold'>Powered by Braga</p>
@@ -222,60 +210,8 @@ export default function Home() {
           
         </section>
 
-        {/* Sectors */}
-        <section ref={sectorsRef} className='whiteBGFullGrid'>
-          {/* Left */}
-          <div className='hidden md:flex flex-col col-start-1 lg:col-start-2 col-span-2 lg:col-span-3 pr-6'>
-            { Sectors.filter((_, idx) => {return(idx%4 == 0 || idx%4 == 1)}).map((sector, index) => (
-              <CardSector 
-                key={index} 
-                sector={sector} 
-                index={index} 
-                selectedSectorTitle={Sectors[selectedSectorIdx].title} 
-              />
-            )) }
-          </div>
-
-          {/* Center */}
-          <div className='col-span-6 md:col-start-3 md:col-span-2 lg:col-start-5 lg:col-span-4'>
-            <h6 className='text-gray-800 text-[20px] lg:text-[22px] mb-6'>We have navigated enterprises and organizations across these sectors.</h6>
-            <div className='w-full flex flex-col sticky top-6 z-10 md:hidden'>
-              { Sectors.map((sector, index) => (
-                <CardSector 
-                  key={index} 
-                  sector={sector} 
-                  index={index} 
-                  selectedSectorTitle={Sectors[selectedSectorIdx].title} 
-                  isMobile={true} 
-                />
-              )) }
-            </div>
-            { Sectors.map((sector, index) => (
-              <div key={index} className='flex flex-col space-y-1 relative z-0'>
-                <button 
-                  key={index} 
-                  className={`${selectedSectorIdx === index ? 'buttonSelectionTimedSelected' : 'buttonSelection'} font-normal relative min-w-[8rem] !flex`} 
-                  onClick={() => setSelectedSectorIdx(index)}
-                >
-                  <Image src={sector.icon} alt='' width={16} height={16} />
-                  <h3 className='text-left mt-4 text-gray-800 ml-3 text-2xl md:text-xl lg:text-3xl '>{sector.title}</h3>
-                </button>
-              </div>
-            )) }
-          </div>
-
-          {/* Right */}
-          <div className='hidden md:flex flex-col col-start-5 col-span-2 lg:col-start-9 lg:col-span-3 pt-36 pl-6'>
-            { Sectors.filter((_, idx) => {return(idx%4 == 2 || idx%4 == 3)}).map((sector, index) => (
-              <CardSector 
-                key={index} 
-                sector={sector} 
-                index={index} 
-                selectedSectorTitle={Sectors[selectedSectorIdx].title} 
-              />
-            )) }
-          </div>
-        </section>
+        {/* SECTORS */}
+        <SectorsSection />
 
         {/* 04 */}
         <section className='blackBG'>
