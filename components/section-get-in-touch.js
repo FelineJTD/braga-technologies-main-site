e6e8d16083f72745a3d3d31bb72ec5a8c5e65018
FelineJTD <16520335@std.stei.itb.ac.id>
2022-09-15 08:10:31 +0700

    fix buttons

--- components/section-get-in-touch.js
+++ components/section-get-in-touch.js
@@ -24,8 +24,8 @@ export default function GetInTouch({number}) {
         <h2 className='mb-4'>Start Mapping Your Future, Today.</h2>
       </div>
       <p className='col-span-3 col-start-7 lg:self-end mb-4 text-gray-600'>Find out how you can maximize the value from data and strengthen your decision making.</p>
-      <Link href='/contact'><a className='col-span-2 mb-4'>
-        <button className='buttonLight lg:self-end'>Contact Us</button>
+      <Link href='/contact'><a className='flex col-span-2 lg:self-end mb-4'>
+        <button className='buttonLight justify-end'>Contact Us &gt;</button>
       </a></Link>
 
 
