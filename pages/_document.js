52558ce8c7a583db606dee3e70275d303662fac2
FelineJTD <16520335@std.stei.itb.ac.id>
2022-09-19 11:03:41 +0700

    erase initial loading

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang="en">
       <Head />
-      <body className='bg-gray-50 text-gray-700'>
-        <div className='absolute h-screen w-full bg-gray-50'>Loading...</div>
+      <body className="bg-gray-50 text-gray-700">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
