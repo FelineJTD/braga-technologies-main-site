d672657131ce06f8a7f0fb2a69e643f8f04a1584
FelineJTD <16520335@std.stei.itb.ac.id>
2022-09-28 20:18:32 +0700

    add last section image to contact

--- config/cfg-contact.js
+++ config/cfg-contact.js
@@ -3,7 +3,7 @@
 
 // LAST SECTION IMAGE
 // This is the background image for the last section.
-export const LastSectionImage = "https://picsum.photos/1080/1920";
+export const LastSectionImage = '/contact/contact.png';
 
 // FORM FIELDS
 // size: 2 for half, 4 for full. Basically it is a 4-column grid. You can get a 1/4 or 3/4 sizes too with 1 or 3.
