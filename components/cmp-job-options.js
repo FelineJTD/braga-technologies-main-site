7b8c18c4103a81516e0d3d4f28908b3518f25616
FelineJTD <16520335@std.stei.itb.ac.id>
2022-09-29 15:53:54 +0700

    add padding to checkbox

--- components/cmp-job-options.js
+++ components/cmp-job-options.js
@@ -16,8 +16,8 @@ export default function JobOptions({option, currWorkplaceTags, currTypeTags, han
       <div className={`${isOpen? 'max-h-[12rem] visible' : 'max-h-0 invisible'} bg-white flex flex-col rounded-lg border-[1px] border-gray-200 absolute mt-2 w-full duration-200 overflow-hidden px-1 z-10`}>
       { option.options.map((subOption, index) => (
         <button key={index} className='flex items-center text-sm font-normal fontVarNormal border-0 even:bg-gray-100 px-2' onClick={() => {handleTagClick(subOption, option.title)}}>
-          <div className={`${currTypeTags.includes(subOption) || currWorkplaceTags.includes(subOption) ? 'bg-gray-500' : ''} border-[1px] rounded-sm border-gray-500 h-4 w-4 mr-2 `}>
-            <Image src='/shared/ic-check.svg' alt='check icon' width={20} height={20} style={{ visibility: (currTypeTags.includes(subOption) || currWorkplaceTags.includes(subOption) ? 'visible' : 'hidden') }} />
+          <div className={`${currTypeTags.includes(subOption) || currWorkplaceTags.includes(subOption) ? 'bg-gray-500' : ''} border-[1px] rounded-sm border-gray-500 h-4 w-4 mr-2 p-0.5`}>
+            <Image src='/shared/ic-check.svg' alt='check icon' width={12} height={12} style={{ visibility: (currTypeTags.includes(subOption) || currWorkplaceTags.includes(subOption) ? 'visible' : 'hidden') }} />
           </div>
           <p className='text-sm text-left'>{ subOption }</p>
         </button>
