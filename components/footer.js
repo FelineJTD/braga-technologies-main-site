242a678c69a48e31c11a4338389935ba5cc43b41
FelineJTD <16520335@std.stei.itb.ac.id>
2022-10-26 18:17:19 +0700

    update footer

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
 import Link from 'next/link'
 
 export default function Footer() {
@@ -10,7 +11,7 @@ export default function Footer() {
           <Link href='/'><img src='/logo-braga-sm.svg' alt='Braga Technologies Logo' className='col-span-6 h-5 cursor-pointer mb-6'/></Link>
           <p className='text-gray-600'>Indonesia&apos;s Leading Geospatial Technology that optimize analytics and performance, moving the world one map at a time.</p>
         </div>
-        <div className='col-span-6 md:col-start-5 lg:col-start-7 lg:col-span-2 flex flex-col space-y-4'>
+        <div className='col-span-6 md:col-start-5 lg:col-start-7 md:col-span-2 flex flex-col space-y-4'>
           <h6 className='text-gray-600'>Product</h6>
           <p className='opacity-50'>GeoDashboard</p>
           {/* <p className='opacity-50'>Braga Enterprise</p>
@@ -31,9 +32,17 @@ export default function Footer() {
         </div>
 
         <div className='col-start-1 col-span-12 border-t-[1px] border-gray-200 mt-4' />
-        <a href='#' className='col-span-6 col-start-1 hidden md:block'><p>Navigate Mutually, with Braga Technologies</p></a>
-        <p className='col-span-8 md:col-span-2 text-gray-500'>Privacy Policy</p>
-        <p className='text-right col-span-4'>{copyrightText}</p>
+        <a href='#' className='col-span-6 col-start-1 hidden lg:block'><p>Navigate Mutually, with Braga Technologies</p></a>
+        <div className='flex gap-3 col-span-8 lg:col-span-4 text-gray-500'>
+          <p>Privacy Policy</p>
+          <a href='https://www.linkedin.com/company/braga-technology/' target='_blank' rel='noopener noreferrer'>
+            <Image src={'/shared/ic-linkedin.svg'} alt='LinkedIn' width={20} height={20} />
+          </a>
+          <a href='https://www.youtube.com/channel/UCkmcfV3OvIojMS5VXzp8tnw' target='_blank' rel='noopener noreferrer'>
+            <Image src={'/shared/ic-youtube.svg'} alt='YouTube' width={20} height={20} />
+          </a>
+        </div>
+        <p className='text-right col-span-2'>{copyrightText}</p>
         <div className='col-start-1 col-span-12 border-t-[1px] border-gray-200' />
         {/* <p className='md:hidden col-span-12'>{copyrightText}</p> */}
       </nav>
