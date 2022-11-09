a20d10ae5163b06de6f2614bc3c50aa90a4a2f9d
FelineJTD <16520335@std.stei.itb.ac.id>
2022-11-09 23:49:17 +0700

    fix logo carousel

--- components/section-partner-logos.js
+++ components/section-partner-logos.js
@@ -3,7 +3,7 @@ import { Logos } from '../config/cfg-partner-logos'
 
 export default function PartnerLogos() {
   return (
-    <section className='w-screen overflow-hidden'>
+    <section className='w-full overflow-hidden'>
       <div className='animate-scrollLeft px-6 xl:px-[10%] py-6 flex justify-around items-center space-x-3 md:space-x-12 bg-gray-50 relative z-10 overflow-auto w-fit hover:[animation-play-state:paused]'>
         <div className='flex justify-around w-1/2 gap-12'>
           {
