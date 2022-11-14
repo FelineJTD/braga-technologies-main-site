fe98cbf38acac9aff2d981dacc98a1c6b3088ddc
FelineJTD <16520335@std.stei.itb.ac.id>
2022-11-14 16:30:05 +0700

    temp fix logo partner

--- components/section-partner-logos.js
+++ components/section-partner-logos.js
@@ -1,19 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 // import Image from "next/image";
 import { Logos } from "../config/cfg-partner-logos";
 
 export default function PartnerLogos() {
   return (
     <section className='w-full overflow-hidden'>
-      <div className='animate-scrollLeft px-6 xl:px-[10%] py-6 flex justify-around items-center space-x-3 md:space-x-12 bg-gray-50 relative z-10 overflow-auto w-fit hover:[animation-play-state:paused]'>
+      <div className='animate-scrollLeft px-6 xl:px-[10%] py-6 flex justify-around items-center space-x-3 md:space-x-12 bg-gray-50 relative overflow-auto z-10 min-w-fit hover:[animation-play-state:paused]'>
         <div className='flex justify-around w-1/2 gap-12'>
           {Logos.map((logo, index) => (
             <div key={index}>
               <img
                 src={logo}
                 alt='Partner Logo'
-                height={96}
-                layout='fixed'
-                objectFit='contain'
+                className='h-8 object-contain max-w-none'
               />
             </div>
           ))}
@@ -24,9 +23,7 @@ export default function PartnerLogos() {
               <img
                 src={logo}
                 alt='Partner Logo'
-                height={96}
-                layout='fixed'
-                objectFit='contain'
+                className='h-8 object-contain max-w-none'
               />
             </div>
           ))}
